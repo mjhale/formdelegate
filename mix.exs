301cd29d51d0b7331a4a99d0d872d8ea8817d56e
Michael Hale <mj504b@gmail.com>
2017-01-01 06:32:42 -0500

    Update Phoenix and related outdated dependencies.

--- mix.exs
+++ mix.exs
@@ -30,16 +30,16 @@ defmodule FormDelegate.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.0"},
+    [{:phoenix, "~> 1.2.1"},
      {:phoenix_pubsub, "~> 1.0"},
      {:phoenix_ecto, "~> 3.0"},
      {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.6"},
-     {:phoenix_live_reload, "~> 1.0", only: :dev},
+     {:phoenix_live_reload, "~> 1.0.6", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
-     {:guardian, "~> 0.12.0"},
-     {:comeonin, "~> 2.5"},
+     {:guardian, "~> 0.14.0"},
+     {:comeonin, "~> 3.0.1"},
      {:ex_machina, "~> 1.0", only: :test},
      {:bamboo, "~> 0.7"},
      {:bamboo_sparkpost, "~> 0.5.0"}]
