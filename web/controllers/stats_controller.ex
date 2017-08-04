f683baa15f3326c8674735a6e9eddbae76bc7725
Michael Hale <mj504b@gmail.com>
2017-08-04 14:58:40 -0400

    Limit message activity to current account.

--- web/controllers/stats_controller.ex
+++ web/controllers/stats_controller.ex
@@ -7,10 +7,10 @@ defmodule FormDelegate.StatsController do
 
   def message_activity(conn, _params) do
     current_account = Guardian.Plug.current_resource(conn)
-    # @TODO Limit to current user's messages
     query = from m in Message,
       right_join: day in fragment("SELECT generate_series(CURRENT_DATE - INTERVAL '10 days', CURRENT_DATE, '1 day') :: date AS d"),
       on: day.d == fragment("date(?)", m.inserted_at),
+      on: m.account_id == ^current_account.id,
       group_by: day.d,
       order_by: day.d,
       select: %{
