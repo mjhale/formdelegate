9ed0c0851cf375e239dd598bb7fac17888648147
Michael Hale <mj504b@gmail.com>
2017-08-05 11:42:16 -0400

    Expose NODE_ENV and limit redux-logger to development mode.

--- web/static/js/app.jsx
+++ web/static/js/app.jsx
@@ -3,15 +3,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createLogger } from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
+import { flowRight as compose } from 'lodash';
 import { Provider } from 'react-redux';
 import { RootRouter } from './router';
 import api from './middleware/api';
 import thunk from 'redux-thunk';
 import reducer from './reducers';
 
-const logger = createLogger();
+const middlewares = [thunk, api];
+if (process.env.NODE_ENV !== 'production') {
+  const logger = createLogger();
 
-const store = createStore(reducer, applyMiddleware(thunk, api, logger));
+  middlewares.push(logger);
+}
+const store = compose(applyMiddleware(...middlewares))(createStore)(reducer);
 
 ReactDOM.render(
   <Provider store={store}>
--- webpack.config.js
+++ webpack.config.js
@@ -52,6 +52,7 @@ module.exports = {
     new CopyWebpackPlugin([{ from: './web/static/assets' }]),
     new ExtractTextPlugin('css/app.css'),
     new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
       API_HOST: JSON.stringify(
         process.env.API_HOST || 'http://localhost:4000/api/'
       ),
