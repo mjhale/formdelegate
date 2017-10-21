71097179a75186666ab87e143211c534b571b22e
Michael Hale <mj504b@gmail.com>
2017-10-21 01:49:18 -0400

    Update Welcome page.

--- web/static/js/components/Welcome.jsx
+++ web/static/js/components/Welcome.jsx
@@ -3,19 +3,54 @@ import React from 'react';
 const Welcome = () => {
   return (
     <div className="welcome">
-      <h1>Simple Form Processing</h1>
-      <p>
-        Form Delegate serves as your forms' intermediate processing provider.
-        Send your existing HTML forms to us and we'll handle the rest.
-      </p>
-      <dt>
-        <li>Step One: Create your form markup</li>
-        <li>
-          Step Two: Add and enable optional integrations for your form on Form
-          Delegate
-        </li>
-        <li>Step Three: Set your form action to your Form Delegate address</li>
-      </dt>
+      <div className="hero">
+        <h1 className="hero-title">Simple Form Processing</h1>
+        <div className="hero-message">
+          Send your forms to us. We'll handle the rest.
+        </div>
+        <div className="hero-action">
+          <button className="btn signup">Try It For Free</button>
+        </div>
+      </div>
+      <div className="action-bar">
+        Want to see it in action? <a href="#">Watch our demo.</a>
+      </div>
+      <div className="features">
+        <h2>Give your forms flexibility</h2>
+        <h3>Add powerful features to your forms. No coding required.</h3>
+        <ul className="feature-list">
+          <li>Send form submissions to multiple e-mail accounts.</li>
+          <li>View and manage every submission with our ad-free interface.</li>
+          <li>Create endless integrations with Zappier.</li>
+          <li>Keep spam out of your life with our automatic filtering.</li>
+        </ul>
+      </div>
+      <div className="cards">
+        <div className="item">
+          <h2>You own your data.</h2>
+          <p>Export or permanently delete your data at any time.</p>
+        </div>
+        <div className="item">
+          <h2>No ads.</h2>
+          <p>
+            Your data isn't used for our monetization. We'll never sell your
+            information.
+          </p>
+        </div>
+        <div className="item">
+          <h2>Strong privacy.</h2>
+          <p>
+            Our guides help you ensure that your data is safe during transit.
+          </p>
+        </div>
+        <div className="item">
+          <h2>Encryption.</h2>
+          <p>
+            Enroll in our client-side PGP beta and encrypt your form
+            submissions.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
