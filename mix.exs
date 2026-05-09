2046865fdc2aa2e7a932e80d3f019376e53148fe
Michael Hale <mail@michaelhale.xyz>
2026-05-09 17:13:07 -0400

    Update dependencies and Elixir and Erlang versions

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule FormDelegate.Mixfile do
     [
       app: :form_delegate,
       version: "0.0.1",
-      elixir: "~> 1.16",
+      elixir: "~> 1.19",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
@@ -36,41 +36,41 @@ defmodule FormDelegate.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.7.12"},
-      {:phoenix_pubsub, "~> 2.1.3"},
-      {:ecto_sql, "~> 3.10"},
-      {:phoenix_ecto, "~> 4.5.1"},
-      {:postgrex, "~> 0.17.0"},
-      {:phoenix_html, "~> 4.1"},
-      {:phoenix_view, "~> 2.0.3"},
-      {:jason, "~> 1.4.1"},
-      {:gettext, "~> 0.24.0"},
-      {:plug_cowboy, "~> 2.7.1"},
-      {:guardian, "~> 2.3.2"},
+      {:phoenix, "~> 1.8.7"},
+      {:phoenix_pubsub, "~> 2.2.0"},
+      {:ecto_sql, "~> 3.13"},
+      {:phoenix_ecto, "~> 4.7.0"},
+      {:postgrex, "~> 0.22.1"},
+      {:phoenix_html, "~> 4.3.0"},
+      {:phoenix_view, "~> 2.0.4"},
+      {:jason, "~> 1.4.5"},
+      {:gettext, "~> 1.0.2"},
+      {:plug_cowboy, "~> 2.8.1"},
+      {:guardian, "~> 2.4.0"},
       {:guardian_phoenix, "~> 2.0"},
       {:pbkdf2_elixir, "~> 2.2"},
       {:bamboo, "~> 2.3"},
       {:bamboo_phoenix, "~> 1.0"},
       {:bamboo_postmark, "~> 1.0"},
-      {:scrivener_ecto, "~> 2.7.0"},
+      {:scrivener_ecto, "~> 3.1.0"},
       {:scrivener_headers, "~> 3.2.2"},
       {:cors_plug, "~> 3.0.3"},
-      {:tesla, "~> 1.9.0"},
+      {:tesla, "~> 1.17.0"},
       {:waffle, "~> 1.1.6"},
       {:waffle_ecto, "~> 0.0.11"},
-      {:hackney, "~> 1.20.1"},
+      {:hackney, "~> 1.21"},
       {:ex_aws, "~> 2.5.0"},
       {:ex_aws_s3, "~> 2.5.2"},
       {:sweet_xml, "~> 0.7.3"},
       {:stripity_stripe, "~> 3.1.0"},
-      {:oban, "~> 2.17"},
+      {:oban, "~> 2.21"},
       {:telemetry, "~> 1.3"},
 
       # dev, test
-      {:ex_machina, "~> 2.7.0", only: :test},
+      {:ex_machina, "~> 2.8.0", only: :test},
       {:credo, "~> 1.7.1", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.4.2", only: [:dev, :test], runtime: false},
-      {:sobelow, "~> 0.13.0", only: [:dev, :test], runtime: false}
+      {:sobelow, "~> 0.14.1", only: [:dev, :test], runtime: false}
     ]
   end
 
